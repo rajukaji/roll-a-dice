cee8a59af8b67b5a2f78910455676708112db19e
Raju Lama <rajulamarainon@gmail.com>
2023-11-18 13:17:12 +0545

    dice and numbers on Js

---
+++ dice.js
@@ -0,0 +1,15 @@
+
+//let a, b, c;
+//to roll a dice
+//console.log(Math.random());
+console.log("hello u");
+
+    document.getElementById("roll").onclick = function () {
+    let a = Math.random() * 6 + 1;// to print random number less than 7 and more than 0
+    let b = Math.random() * 6 + 1;
+    let c = Math.random() *6 + 1;
+
+    document.getElementById("aLabel").innerHTML = Math.floor(a);
+    document.getElementById("bLabel").innerHTML = Math.floor(b);
+    document.getElementById("cLabel").innerHTML = Math.floor(c);
+};
---
+++ max.js
@@ -0,0 +1,16 @@
+let x, y, z;
+document.getElementById("enter").onclick = function () {
+
+    x = document.getElementById("xText").value;
+    x = Number(x);
+    y = document.getElementById("yText").value;
+    y = Number(y);
+    z = document.getElementById("zText").value;
+    z = Number(z);
+
+    console.log(Math.max(x, y, z)); //to only show in the console
+
+    document.getElementById("print").innerHTML = "The greatest Number is " + (Math.max(x, y, z)); //to show in the webpage/dom
+
+
+};
