a767276297f05b09b2e865be7ecee24ff5216658
Raju Lama <rajulamarainon@gmail.com>
2023-11-18 18:45:14 +0545

    new code

---
+++ increaseOrDecrease.js
@@ -0,0 +1,13 @@
+let count, increase, reset, decrease;
+document.getElementById().value = function(){
+    count = 0;
+    reset = 0;
+    increase = ++count;
+    decrease = --count;
+    document.getElementById().innerHTML = increase;
+
+    document.getElementById().innerHTML = decrease;
+
+    document.getElementById().innerHTML = reset;
+
+};
